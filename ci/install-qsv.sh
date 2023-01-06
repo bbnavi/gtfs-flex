a11039aedff567f706084dd24373aaac06fb0497
Jannis R <mail@jannisr.de>
2023-01-06 15:11:54 +0100

    CI: use qsv 0.81.0, improve qsv download 💚

--- ci/install-qsv.sh
+++ ci/install-qsv.sh
@@ -8,19 +8,19 @@ sudo apt install -y \
 	curl jq unzip \
 	libpython3.10
 
-qsv_release='78436643' # 0.69.0
+qsv_release='87693052' # 0.81.0
 releases_url="https://api.github.com/repos/jqnatividad/qsv/releases/$qsv_release"
 assets_url="$(
-	curl "$releases_url" -H 'Accept: application/json' -L -s \
+	curl "$releases_url" -H 'Accept: application/json' -L -fsS \
 	| jq -r '.assets_url'
 )"
 qsv_x64_url="$(
-	curl "$assets_url" -H 'Accept: application/json' -L -s \
+	curl "$assets_url" -H 'Accept: application/json' -L -fsS \
 	| jq -r '.[] | select(.name | test("qsv-[0-9.]+-x86_64-unknown-linux-gnu.zip")) | .browser_download_url'
 )"
 
 zip="$(mktemp)"
-curl -o "$zip" "$qsv_x64_url" -L -s
+curl -o "$zip" "$qsv_x64_url" -L -fsS
 
 mkdir -p "$HOME/.local/bin"
 unzip -j -q -d "$HOME/.local/bin" "$zip" qsv
