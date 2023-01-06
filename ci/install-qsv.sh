3e32f772e48fcf50fb0d00c846d7ee9393eb5936
Jannis R <mail@jannisr.de>
2023-01-06 15:14:44 +0100

    CI: let curl send a reasonable User-Agent 💚

--- ci/install-qsv.sh
+++ ci/install-qsv.sh
@@ -8,19 +8,20 @@ sudo apt install -y \
 	curl jq unzip \
 	libpython3.10
 
+ua='bbnavi/gtfs-flex CI'
 qsv_release='87693052' # 0.81.0
 releases_url="https://api.github.com/repos/jqnatividad/qsv/releases/$qsv_release"
 assets_url="$(
-	curl "$releases_url" -H 'Accept: application/json' -L -fsS \
+	curl "$releases_url" -H 'Accept: application/json' -H "User-Agent: $ua" -L -fsS \
 	| jq -r '.assets_url'
 )"
 qsv_x64_url="$(
-	curl "$assets_url" -H 'Accept: application/json' -L -fsS \
+	curl "$assets_url" -H 'Accept: application/json' -H "User-Agent: $ua" -L -fsS \
 	| jq -r '.[] | select(.name | test("qsv-[0-9.]+-x86_64-unknown-linux-gnu.zip")) | .browser_download_url'
 )"
 
 zip="$(mktemp)"
-curl -o "$zip" "$qsv_x64_url" -L -fsS
+curl -o "$zip" "$qsv_x64_url" -H "User-Agent: $ua" -L -fsS
 
 mkdir -p "$HOME/.local/bin"
 unzip -j -q -d "$HOME/.local/bin" "$zip" qsv
